5bebd49dceb5865edab93fe8c9dcebea2e517dfa
Test <143610555+fish-12345@users.noreply.github.com>
2026-01-10 14:09:03 +0300

    Add files via upload
    Signed-off-by: Test <143610555+fish-12345@users.noreply.github.com>

--- app/src/main/java/io/github/dovecoteescapee/byedpi/data/AppInfo.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/data/AppInfo.kt
@@ -5,6 +5,6 @@ import android.graphics.drawable.Drawable
 data class AppInfo(
     val appName: String,
     val packageName: String,
-    val icon: Drawable,
+    val icon: Drawable?,
     var isSelected: Boolean
 )
